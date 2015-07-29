a0bf0e1bc6a2d1b0e9754f4f56976c5967026fee
Lincoln Brito <lincoln.sbrito@gmail.com>
2015-07-29 17:11:26 -0300

    mudando indentação

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ var sass = require('gulp-sass');
 
 //definindo a ordem de concatenação
 var js_files = [
-	'src/js/jquery/*.js',
-	'src/js/bootstrap/*.js',
-	'src/js/creative/*.js'
+    'src/js/jquery/*.js',
+    'src/js/bootstrap/*.js',
+    'src/js/creative/*.js'
 ]
 
 var sass_files = [
-	'src/sass/**/*.scss'	
+    'src/sass/**/*.scss'    
 ]
 
 var paths = {
@@ -27,31 +27,31 @@ var paths = {
 }
 
 gulp.task('scripts', function(){
-	return gulp
-				.src( js_files )
-				.pipe(concat('app.js'))
-				.pipe(uglify())
-				.pipe(gulp.dest('build/js'));
+    return gulp
+                .src( js_files )
+                .pipe(concat('app.js'))
+                .pipe(uglify())
+                .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('sass', function(){
-	return gulp
-				.src(sass_files)
-				.pipe(sass({
-			        outputStyle: 'compressed',
-			        //sourceComments: 'map',
-			        includePaths : [paths.styles.src]
-			    }))
-				//.pipe(gulp.dest('css'))
-				//.pipe(concat('main.css'))
-				.pipe(gulp.dest('build/css'));
+    return gulp
+                .src(sass_files)
+                .pipe(sass({
+                    outputStyle: 'compressed',
+                    //sourceComments: 'map',
+                    includePaths : [paths.styles.src]
+                }))
+                //.pipe(gulp.dest('css'))
+                //.pipe(concat('main.css'))
+                .pipe(gulp.dest('build/css'));
 })
 
 gulp.task('watch', function(){
-	//gulp.watch('src/**/*.js', function(event){
-	//	gutil.log('File '+event.path+' was '+event.type+', running tasks...');
-	//	gulp.run('scripts');
-	//});
-	gulp.watch(['src/**/*.js','src/sass/*.scss'],['scripts']);
-	gulp.watch(['src/sass/*.scss'],['sass']);
+    //gulp.watch('src/**/*.js', function(event){
+    //  gutil.log('File '+event.path+' was '+event.type+', running tasks...');
+    //  gulp.run('scripts');
+    //});
+    gulp.watch(['src/**/*.js','src/sass/*.scss'],['scripts']);
+    gulp.watch(['src/sass/*.scss'],['sass']);
 });
