d3d36ef173aea2648bd069b3205b3987462c0d8e
Lincoln Brito <lincoln.sbrito@gmail.com>
2015-07-29 16:31:18 -0300

    definindo a ordem de concatenação

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,19 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+
+//definindo a ordem de concatenação
+var files = [
+	'src/js/jquery/*.js',
+	'src/js/bootstrap/*.js',
+	'src/js/creative/*.js'
+]
 
 gulp.task('scripts', function(){
 	return gulp
-				.src(['src/js/**/*.js'])
+				.src( files )
+				.pipe(concat('app.js'))
 				.pipe(uglify())
 				.pipe(gulp.dest('build/js'));
 });
